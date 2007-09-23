29a71993bb6cf710dbfe5234bdd866c81248ed6e
Ryan C. Gordon <icculus@icculus.org>
2007-09-23 09:05:34 +0000

    Papertrail output.

---
+++ php/papertrail.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'saysomethingnice.php';
+
+function render_papertrail()
+{
+    // !!! FIXME: forbid this output to non-admins...
+
+    // !!! FIXME: make a UI toggle for this.
+    //if (!get_input_bool('showsql', 'should show sql', $showsql, 'y')) return;
+    showsql = true;
+
+    $sql = 'select * from papertrail order by entrydate';
+    $query = do_dbquery($sql);
+    if ($query == false)
+        return;  // error output is handled in database.php ...
+
+    echo "<u>Current paper trail (oldest entries first)...</u>\n";
+    echo "<ul>\n";
+    
+    while ( ($row = db_fetch_array($query)) != false )
+    {
+        $htmlaction = htmlentities($row['action'], ENT_QUOTES);
+        $htmlauthor = htmlentities($row['author'], ENT_QUOTES);
+        $htmlentrydate = htmlentities($row['entrydate'], ENT_QUOTES);
+        $htmlsql = '';
+        if ($showsql)
+            $htmlsql = "<br>\n<code>'" . htmlentities($row['sql'], ENT_QUOTES) . "'</code>";
+        echo "  <li><b>$htmlaction</b>: <i>by $htmlauthor, on ${htmlentrydate}</i>${htmlsql}\n";
+    } // while
+    db_free_result($query);
+
+    echo "</ul>\n";
+    echo "<p>End of papertrail.\n";
+} // op_renderpapertrail
+
+// The mainline...
+render_header();
+render_papertrail();
+render_footer();
+
+?>
