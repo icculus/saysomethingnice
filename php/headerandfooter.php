93acece5c3012da801d1198070ccbef3e210e92b
Ryan C. Gordon <icculus@icculus.org>
2007-09-23 08:36:33 +0000

    Some posting stuff. Incomplete.

--- php/headerandfooter.php
+++ php/headerandfooter.php
@@ -5,7 +5,7 @@ require_once 'common.php';
 function render_header($title = 'Quick, say something nice!')
 {
 // !!! FIXME: need more here, I guess.
-echo <<< EOF
+echo <<<EOF
 <html><head><title>$title</title></head><body>
 <center><h1>Say Something Nice</h1></center>
 
@@ -15,7 +15,7 @@ EOF;
 function render_footer()
 {
 // !!! FIXME: need more here, I guess.
-echo <<< EOF
+echo <<<EOF
 <hr>
 <center>
   [ <a href="post.php">Add a quote</a> | <a href="http://1-800-flowers.com/">cover your ass</a> ]<br>
