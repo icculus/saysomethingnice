34dac69ae5906dba673d5f58e21174e9a66012fd
icculus <icculus@14eb450f-c63a-0410-92a0-f3d076779e99>
2007-09-23 06:39:36 +0000

    Just a database dump for now.

--- trunk/php/index.php
+++ trunk/php/index.php
@@ -4,6 +4,28 @@ require_once 'database.php';
 require_once 'headerandfooter.php';
 
 render_header();
+
+$query = do_dbquery('select * from quotes;');
+if ($query == false)
+    write_error('query failed');
+else
+{
+    echo "<table>\n";
+    while ( ($row = db_fetch_array($query)) != false )
+    {
+        echo "  <tr>\n");
+        echo "    <td>id: ${row['id']}</td>\n");
+        echo "    <td>text: ${row['text']}</td>\n");
+        echo "    <td>public: ${row['public']}</td>\n");
+        echo "    <td>author: ${row['author']}</td>\n");
+        echo "    <td>entrydate: ${row['entrydate']}</td>\n");
+        echo "    <td>lastedit: ${row['lastedit']}</td>\n");
+        echo "  </tr>\n");
+    }
+    echo "</table>\n";
+    echo "<hr>\n";
+}
+
 echo "<center>blah!</center>\n";  // !!! FIXME: do some magic here.
 render_footer();
 ?>
