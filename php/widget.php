00307865a4cd8ab0bd149a8e44bf3a71c089be66
Ryan C. Gordon <icculus@icculus.org>
2008-01-27 11:48:31 +0000

    Added a widget output program.

---
+++ php/widget.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'saysomethingnice.php';
+
+// The mainline...
+// This is ALL this does...it doesn't output the <html> tags or anything;
+//  the widget handles that.
+render_random_quote();
+
+?>
+
