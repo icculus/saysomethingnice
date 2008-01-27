55d8eaa7cbae4e0afebe3f8fa372d2b568db4cb4
Ryan C. Gordon <icculus@icculus.org>
2008-01-27 09:12:20 +0000

    Reenable login requirement.

--- php/initialize.php
+++ php/initialize.php
@@ -2,11 +2,11 @@
 
 require_once 'saysomethingnice.php';
 
-//if (!valid_admin_login())
-//{
-//    admin_login_prompt();
-//    exit(0);
-//} // if
+if (!valid_admin_login())
+{
+    admin_login_prompt();
+    exit(0);
+} // if
 
 $always_show_papertrail = true;  // always for this page.
 
