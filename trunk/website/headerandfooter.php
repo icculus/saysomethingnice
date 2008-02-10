a71fdb5a7a74cc91f778f18a7a8829ebff779da7
publish <publish@14eb450f-c63a-0410-92a0-f3d076779e99>
2008-02-10 04:16:41 +0000

    Fix xhtml validation whining.

--- trunk/website/headerandfooter.php
+++ trunk/website/headerandfooter.php
@@ -12,11 +12,14 @@ function render_header($title=NULL, $headextras='', $showads=true)
     if ($title == NULL)
         $title = 'Quick, say something nice!';
 
+    header('Content-Type: text/html;charset=utf-8');
+
     $advertisements = $showads ? get_advertisements() : '';
 
     echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' .
          "<html xmlns='http://www.w3.org/1999/xhtml'>" .
            "<head>" .
+             "<meta http-equiv='Content-Type' content='text/html;charset=utf-8' />" .
              $headextras .
              "<title>$title</title>" .
              "<link rel='alternate' type='application/rss+xml'" .
