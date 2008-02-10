2f65c66575f07d488cea874718825e1ddf5510c1
Ryan C. Gordon <icculus@icculus.org>
2008-02-10 03:35:44 +0000

    New web design!

--- php/headerandfooter.php
+++ php/headerandfooter.php
@@ -7,27 +7,27 @@ function render_header($title=NULL, $headextras='', $showads=true)
     $rssurl = get_rss_url();
     $posturl = get_post_url();
     $cssurl = get_css_url();
+    $imgurl = get_static_imgdir_url();
 
     if ($title == NULL)
         $title = 'Quick, say something nice!';
 
     $advertisements = $showads ? get_advertisements() : '';
 
-// !!! FIXME: need more here, I guess.
-echo <<<EOF
-<html>
-  <head>$headextras
-    <title>$title</title>
-    <link rel="alternate" type="application/rss+xml" title="Speed Feed" href="${rssurl}" />
-    <link rel="stylesheet" type="text/css" href="${cssurl}" />
-  </head>
-  <body>
-  <center>
-    $advertisements
-    <h1>Quick, Say Something Nice!</h1>
-  </center>
-
-EOF;
+    echo "<html>" .
+           "<head>" .
+             $headextras .
+             "<title>$title</title>" .
+             "<link rel='alternate' type='application/rss+xml'" .
+             " title='Speed Feed' href='${rssurl}' />" .
+             //"<link rel='stylesheet' type='text/css' href='${cssurl}' />" .
+           "</head>" .
+           "<body style='text-align: center'>" .
+             $advertisements .
+             "<p>" .
+               "<img src='${imgurl}header.jpg'" .
+               " alt='Quick, Say Something Nice!' />" .
+             "</p>";
 
     write_debug('If you can read this, debugging is enabled!');
 } // render_header
@@ -37,20 +37,15 @@ function render_footer()
 {
     $baseurl = get_base_url();
     $posturl = get_post_url();
+    $widgeturl = get_widget_url();
 
-// !!! FIXME: need more here, I guess.
-echo <<<EOF
-<hr>
-<center>
-  [ <a href="${baseurl}">Get a quote</a> |
-    <a href="${posturl}">Add a quote</a> |
-    <a href="${baseurl}downloads/SaySomethingNice.wdgt.zip">Get Mac Widget</a>
-  ]<br>
-</center>
-
-</body></html>
-
-EOF;
+    echo     "<a href='${baseurl}'>Get a quote</a>" .
+             "&nbsp;&nbsp;&nbsp;" .
+             "<a href='${posturl}'>Add a quote</a>" .
+             //"&nbsp;&nbsp;" .
+             //"<a href='${widgeturl}'>Get Mac Widget</a>" .
+           "</body>" .
+         "</html>";
 } // render_footer
 
 ?>
