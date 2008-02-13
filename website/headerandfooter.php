d7a71fb584b913f7850f2cb00f49a1a643ec9faf
Ryan C. Gordon <icculus@icculus.org>
2008-02-13 00:49:22 +0000

    Don't cache so new quote loads on front page every time.

--- website/headerandfooter.php
+++ website/headerandfooter.php
@@ -13,6 +13,7 @@ function render_header($title=NULL, $headextras='', $showads=true)
         $title = 'Quick, say something nice!';
 
     header('Content-Type: text/html;charset=utf-8');
+    header('Cache-Control: no-cache');
 
     $advertisements = $showads ? get_advertisements() : '';
 
@@ -20,6 +21,7 @@ function render_header($title=NULL, $headextras='', $showads=true)
          "<html xmlns='http://www.w3.org/1999/xhtml'>" .
            "<head>" .
              "<meta http-equiv='Content-Type' content='text/html;charset=utf-8' />" .
+             "<meta http-equiv='Cache-Control' content='no-cache' />" .
              $headextras .
              "<title>$title</title>" .
              "<link rel='alternate' type='application/rss+xml'" .
