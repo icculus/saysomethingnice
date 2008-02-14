be65a0f3265091a4975cefd3a0e557d3c21f8fe6
Ryan C. Gordon <icculus@icculus.org>
2008-02-14 06:15:52 +0000

    GeoIP support.

--- website/admin/admin.php
+++ website/admin/admin.php
@@ -1,9 +1,95 @@
 <?php
 
 require_once '../saysomethingnice.php';
+require_once 'geoip/geoipcity.inc';
+require_once 'geoip/geoipregionvars.php';
 
 $adminurl = get_admin_url();
 $logouturl = $adminurl . "?action=logout";
+
+function output_tooltip_script()
+{
+    // !!! FIXME: this should really be in tooltip.js instead.
+echo <<< EOF
+
+<div id="dhtmltooltip"></div>
+
+<script type="text/javascript">
+
+/***********************************************
+* Cool DHTML tooltip script- © Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/
+
+var offsetxpoint=-60 //Customize x offset of tooltip
+var offsetypoint=20 //Customize y offset of tooltip
+var ie=document.all
+var ns6=document.getElementById && !document.all
+var enabletip=false
+if (ie||ns6)
+var tipobj=document.all? document.all["dhtmltooltip"] : document.getElementById? document.getElementById("dhtmltooltip") : ""
+
+function ietruebody(){
+return (document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body
+}
+
+function ddrivetip(thetext, thecolor, thewidth){
+if (ns6||ie){
+if (typeof thewidth!="undefined") tipobj.style.width=thewidth+"px"
+if (typeof thecolor!="undefined" && thecolor!="") tipobj.style.backgroundColor=thecolor
+tipobj.innerHTML=thetext
+enabletip=true
+return false
+}
+}
+
+function positiontip(e){
+if (enabletip){
+var curX=(ns6)?e.pageX : event.clientX+ietruebody().scrollLeft;
+var curY=(ns6)?e.pageY : event.clientY+ietruebody().scrollTop;
+//Find out how close the mouse is to the corner of the window
+var rightedge=ie&&!window.opera? ietruebody().clientWidth-event.clientX-offsetxpoint : window.innerWidth-e.clientX-offsetxpoint-20
+var bottomedge=ie&&!window.opera? ietruebody().clientHeight-event.clientY-offsetypoint : window.innerHeight-e.clientY-offsetypoint-20
+
+var leftedge=(offsetxpoint<0)? offsetxpoint*(-1) : -1000
+
+//if the horizontal distance isn't enough to accomodate the width of the context menu
+if (rightedge<tipobj.offsetWidth)
+//move the horizontal position of the menu to the left by it's width
+tipobj.style.left=ie? ietruebody().scrollLeft+event.clientX-tipobj.offsetWidth+"px" : window.pageXOffset+e.clientX-tipobj.offsetWidth+"px"
+else if (curX<leftedge)
+tipobj.style.left="5px"
+else
+//position the horizontal position of the menu where the mouse is positioned
+tipobj.style.left=curX+offsetxpoint+"px"
+
+//same concept with the vertical position
+if (bottomedge<tipobj.offsetHeight)
+tipobj.style.top=ie? ietruebody().scrollTop+event.clientY-tipobj.offsetHeight-offsetypoint+"px" : window.pageYOffset+e.clientY-tipobj.offsetHeight-offsetypoint+"px"
+else
+tipobj.style.top=curY+offsetypoint+"px"
+tipobj.style.visibility="visible"
+}
+}
+
+function hideddrivetip(){
+if (ns6||ie){
+enabletip=false
+tipobj.style.visibility="hidden"
+tipobj.style.left="-1000px"
+tipobj.style.backgroundColor=''
+tipobj.style.width=''
+}
+}
+
+document.onmousemove=positiontip
+
+</script>
+
+EOF;
+} // output_tooltip_script
+
 
 function get_admin_names()
 {
@@ -21,7 +107,17 @@ function get_admin_names()
 
 function output_quote_queue_rows($category, $showall = 0)
 {
-    global $adminurl;
+    global $adminurl, $geoipdb, $GEOIP_REGION_NAME;
+
+    $gi = NULL;
+
+    if (isset($geoipdb))
+    {
+        // uncomment for Shared Memory support
+        // geoip_load_shared_mem($geoipdb);
+        // $gi = geoip_open($geoipdb,GEOIP_SHARED_MEMORY);
+        $gi = geoip_open($geoipdb,GEOIP_STANDARD);
+    } // if
 
     $sql = "select * from quotes where category=$category";
     if (!$showall)  // show only pending?
@@ -64,6 +160,21 @@ function output_quote_queue_rows($category, $showall = 0)
 
         $ip = long2ip($row['ipaddr']);
 
+        if (isset($gi))
+        {
+            $record = geoip_record_by_addr($gi, $ip);
+            $ip = "<div onMouseover=\"ddrivetip('" . 
+                  "country: " . escapehtml($record->country_name) . "<br/>" .
+                  "region: " . escapehtml($GEOIP_REGION_NAME[$record->country_code][$record->region]) . "<br/>" .
+                  "city: " . escapehtml($record->city) . "<br/>" .
+                  "latitude: " . escapehtml($record->latitude) . "<br/>" .
+                  "longitude: " . escapehtml($record->longitude) . "<br/>" .
+                  "zip code: " . escapehtml($record->postal_code) . "<br/>" .
+                  "dma code: " . escapehtml($record->dma_code) . "<br/>" .
+                  "area code :" . escapehtml($record->area_code) . "<br/>" .
+                  "', 'yellow', 300);\" onMouseout=\"hideddrivetip();\">$ip</div>";
+        } // if
+
         print("<tr>\n");
         print('<td align="center"> <input type="checkbox" name="itemid[]"');
         print(" value=\"{$row['id']}\"></td>\n");
@@ -95,6 +206,8 @@ function output_quote_queue_widgets()
     // !!! FIXME: 'q' is a leftover from IcculusNews queues.
     get_input_int('q', 'Category ID number', $q, 0);
     get_input_bool('showall', 'show all posts', $showall, 'false');
+
+    output_tooltip_script();
 
     $showallflip = 1;
     $showalltext = "Show all items";
