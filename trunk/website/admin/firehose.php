9be30ddcc931d9331ff3473fc8ad4163ec40a7d0
icculus <icculus@14eb450f-c63a-0410-92a0-f3d076779e99>
2008-02-14 04:42:23 +0000

    Hopefully fix firehose URLs.

--- trunk/website/admin/firehose.php
+++ trunk/website/admin/firehose.php
@@ -11,15 +11,15 @@ if (!valid_admin_login())
 $sql = 'select * from quotes order by postdate desc limit 15';
 $title = 'Say Something Nice Admin Firehose';
 $desc = 'Look here to filter out people looking for a booty call.';
-$baseurl = get_admin_url();
+$adminurl = get_admin_url();
 $rssurl = get_firehose_url();
 
 function firehosecallback($id)
 {
-    global $baseurl;
-    return "${baseurl}?action=edit&id=$id";
+    global $adminurl;
+    return "${adminurl}?action=edit&id=$id";
 } // firehosecallback
 
-do_rss($sql, $baseurl, $rssurl, $title, $desc, 'firehosecallback');
+do_rss($sql, $adminurl, $rssurl, $title, $desc, 'firehosecallback');
 
 ?>
