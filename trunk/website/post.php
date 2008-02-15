6381a5849b6434ef4680b53e583c8217c009ad90
publish <publish@14eb450f-c63a-0410-92a0-f3d076779e99>
2008-02-15 21:16:49 +0000

    More adsense wanking.

--- trunk/website/post.php
+++ trunk/website/post.php
@@ -25,14 +25,18 @@ function render_submission_ui()
 
 echo <<< EOF
     $form
+    <!-- google_ad_section_start -->
     <p>Go ahead and add a compliment, apology, encouragement, or some other sweetness.<br/>
     It will show up on the front page once the crew comes along
     and makes sure it isn't spam or anything.</p>
+    <!-- google_ad_section_end -->
+    <!-- google_ad_section_start(weight=ignore) -->
     <p>
     Your quote: <input type='text' size="60" name='quote'><br>
     (optional) email address: <input type='text' size="20" name='author'><br>
     <input type='submit' name='submitting' value='Go!'>
     </p>
+    <!-- google_ad_section_end -->
     </form>
 
 EOF;
