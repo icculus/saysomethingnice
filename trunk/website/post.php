4832cc7bb544645f715e1a3c6f69db1d69db498a
publish <publish@14eb450f-c63a-0410-92a0-f3d076779e99>
2008-02-22 07:13:09 +0000

    Limit post size.

--- trunk/website/post.php
+++ trunk/website/post.php
@@ -10,6 +10,17 @@ function process_possible_submission()
     if (!get_input_string('quote', 'Quote text', $quote)) return;
     if (!get_input_string('author', 'Email address', $author, '', true)) return;
 
+    if (strlen($quote) > 512)
+    {
+        $escaped = escapehtml($quote);
+        write_error("Sorry, your quote is too long.");
+        echo "<center>\"$escaped\"</center><hr>\n";
+        return;
+    } // if
+
+    if (strlen($author) > 128)  // just trim this, no need to fight.
+        $author = substr($author, 0, 128);
+
     write_debug('Got an apparently non-bogus submission!');
     if (add_quote($quote, $author, ip2long($_SERVER['REMOTE_ADDR'])))
     {
@@ -32,8 +43,8 @@ echo <<< EOF
     <!-- google_ad_section_end -->
     <!-- google_ad_section_start(weight=ignore) -->
     <p>
-    Your quote: <input type='text' size="60" name='quote'><br>
-    (optional) email address: <input type='text' size="20" name='author'><br>
+    Your quote: <input type='text' size='60' maxlength='512' name='quote'><br>
+    (optional) email address: <input type='text' size='20' maxlength='128' name='author'><br>
     <input type='submit' name='submitting' value='Go!'>
     </p>
     <!-- google_ad_section_end -->
